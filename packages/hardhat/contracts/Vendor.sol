05cd8acb2d20a08818ecdb8026e0ddff9ebb2d77
yeungparkhay <yeungparkhay@gmail.com>
2022-06-14 17:59:07 +0100

    initial commit

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,20 +1,20 @@
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor {
+    YourToken yourToken;
 
-  YourToken yourToken;
+    constructor(address tokenAddress) public {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) public {
-    yourToken = YourToken(tokenAddress);
-  }
+    //ToDo: create a payable buyTokens() function:
 
-  //ToDo: create a payable buyTokens() function:
+    //ToDo: create a sellTokens() function:
 
-  //ToDo: create a sellTokens() function:
-
-  //ToDo: create a withdraw() function that lets the owner, you can 
-  //use the Ownable.sol import above:
+    //ToDo: create a withdraw() function that lets the owner, you can
+    //use the Ownable.sol import above:
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,9 +1,15 @@
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
-contract YourToken {
-    //ToDo: add constructor and mint tokens for deployer,
-    //you can use the above import for ERC20.sol. Read the docs ^^^
+contract YourToken is ERC20 {
+    address public deployer;
+
+    constructor() public ERC20("YourToken", "YT") {
+        deployer = msg.sender;
+        _mint(msg.sender, 1000 * 10**18);
+    }
 }
